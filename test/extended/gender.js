cbd1121c20b3c6bbe53586443d13803bee8293c6
Mykhailo Sheptun <michael.sheptun@gmail.com>
2021-03-31 00:51:28 -0400

    created TC 050

--- test/extended/gender.js
+++ test/extended/gender.js
@@ -13,9 +13,53 @@ describe('Gender suite with radio buttons', function (){
         browser.refresh()
         $$(selGen.gender)[gender.he].click();
         let heButton = $$(selGen.gender)[gender.he].isEnabled()
-        browser.pause(2000)
         expect(heButton).toEqual(true)
 
     })
 
+    it('TC-048 Verify that button "she" is enabled', function (){
+        browser.refresh()
+        $$(selGen.gender)[gender.she].click();
+        let sheButton = $$(selGen.gender)[gender.she].isEnabled()
+        expect(sheButton).toEqual(true)
+
+    })
+
+    it('TC-049 Verify that button "it" is enabled', function (){
+        browser.refresh()
+        $$(selGen.gender)[gender.it].click();
+        let itButton = $$(selGen.gender)[gender.it].isEnabled()
+        expect(itButton).toEqual(true)
+
+    })
+
+    it('TC-050 User can choose only one button at the time: "he"', () => {
+         browser.refresh()
+        let elem = $$(selGen.gender)[gender.he]
+        let isEnabled = elem.isEnabled();
+        console.log(isEnabled); // outputs: true
+
+        elem = $$(selGen.gender)[gender.she]
+        isEnabled = elem.isEnabled();
+        console.log(isEnabled); // outputs: false
+
+        elem = $$(selGen.gender)[gender.it]
+        isEnabled = elem.isEnabled();
+        console.log(isEnabled); // outputs: false
+    });
+
+    it('TC-051 User can choose only one button at the time: "she"', () => {
+        browser.refresh()
+        let elem = $$(selGen.gender)[gender.she]
+        let isEnabled = elem.isEnabled();
+        console.log(isEnabled); // outputs: true
+
+        elem = $$(selGen.gender)[gender.he]
+        isEnabled = elem.isEnabled();
+        console.log(isEnabled); // outputs: false
+
+        elem = $$(selGen.gender)[gender.it]
+        isEnabled = elem.isEnabled();
+        console.log(isEnabled); // outputs: false
+    });
 })
